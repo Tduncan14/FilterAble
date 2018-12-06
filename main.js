af1e47e2f9dc0624a800ae089b923e86ff103b4a
unknown <nbatre1@yahoo.com>
2018-12-05 23:11:13 -0600

    keep moving forward

--- main.js
+++ main.js
@@ -15,5 +15,32 @@ function filterNames(){
     let filterValue = document.getElementById('filterInput').value.toUpperCase();
 
     console.log(filterValue);
+
+    // Get names by ul
+
+    let ul = document.getElementById('names');
+
+    // get the items li
+
+    let li = ul.querySelectorAll('li.collection-item');
+
+
+    // Loops through the collection -item lis
+
+    for(var i = 0 ; i < li.length; i++){
+
+        let a = li[i].getElementsByTagName('a')[0];
+        // If matches
+
+        if(a.innerHTML.toUpperCase().indexOf(filterValue) > -1){
+            
+           li[i].style.display='';
+        }
+        .
+
+        else{
+            li[i].style.display = 'none';
+        }
+    }
 }
 
